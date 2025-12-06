800d14b267022ffe782bac534f9734eac416cdf7
Erzis900 <erzis900@gmail.com>
2025-12-06 22:33:10 +0100

    basic miner states

--- world/miner/miner.gd
+++ world/miner/miner.gd
@@ -1,1 +1,49 @@
 class_name Miner extends Area2D
+
+# exports
+@export var movement_speed: float = 100
+
+# children
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var mining_timer: Timer = $MiningTimer
+
+# private
+enum States { IDLE, WALKING, MINING }
+var state: States = States.WALKING
+var direction: Vector2 = Vector2.RIGHT
+
+
+func _physics_process(delta: float) -> void:
+	if state == States.WALKING:
+		walk(delta)
+	elif state == States.MINING:
+		mine()
+
+
+func walk(delta: float) -> void:
+	position += direction * movement_speed * delta
+	animated_sprite.play("walk_right")
+
+
+func mine() -> void:
+	animated_sprite.play("mine")
+
+
+func _on_stone_wall_area_entered(_area: Area2D) -> void:
+	mining_timer.start()
+	state = States.MINING
+
+
+func _on_lift_area_entered(_area: Area2D) -> void:
+	change_walk_direction()
+
+
+func change_walk_direction() -> void:
+	direction.x *= -1
+	animated_sprite.flip_h = !animated_sprite.flip_h
+
+
+func _on_mining_timer_timeout() -> void:
+	mining_timer.stop()
+	change_walk_direction()
+	state = States.WALKING
